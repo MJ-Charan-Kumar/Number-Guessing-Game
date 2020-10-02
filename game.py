6ee8203c32e0e04b38a98a820af3395e07205801
MJ-Charan-Kumar <69426600+MJ-Charan-Kumar@users.noreply.github.com>
2020-10-02 19:51:19 +0530

    Add files via upload

---
+++ game.py
@@ -0,0 +1,29 @@
+import random
+name = input('Hello, What is your name?')
+
+number = random.randint(1,30)
+
+print(name + ', enter a number between 0 and 30')
+tries = 0
+win = 0
+print('Guess the number within 5 guesses! ')
+
+while tries < 5:
+	tries += 1
+	print("Try Number:", tries, end=" ")
+	guess = int(input())
+
+
+	if guess < number and tries != 5:
+		print('The number is too low, try again')
+
+	if guess > number:	
+		print('The number is too high, try again')
+
+	if guess == number:
+		print('Well done, you guessed it right')
+		win=1
+		break
+
+if win == 0:
+	print('You lost,' + name)
